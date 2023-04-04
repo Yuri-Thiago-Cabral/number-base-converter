869ce02556e091e5f1b745e4351130b2155bc524
Yuri Thiago Cabral <yurithiago100@gmail.com>
2023-04-04 12:35:35 -0300

    feat: implement hex converter

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -1,10 +1,55 @@
 class Functions {
-  static String convertDecimalToBinary(String decimal) {
-    return decimal.isNotEmpty ? int.parse(decimal).toRadixString(2) : '';
+  // Convert to binary
+  static String convertDecimalToBinary(String sequence) {
+    var result = _getResult(sequence, 2);
+    return result;
   }
 
-  static String convertBinaryToDecimal(String binary) {
-    var result = int.tryParse(binary, radix: 2);
-    return result != null ? result.toString() : '';
+  static String convertHexadecimalToBinary(String sequence) {
+    var decimalSequence = convertHexadecimalToDecimal(sequence);
+    var result = convertDecimalToBinary(decimalSequence);
+    return result;
+  }
+
+  // Convert to decimal
+  static String convertBinaryToDecimal(String sequence) {
+    var result = _getResultExplicitRadix(sequence, 2);
+    return result;
+  }
+
+  static String convertHexadecimalToDecimal(String sequence) {
+    var result = _getResultExplicitRadix(sequence, 16);
+    return result;
+  }
+
+  // Convert to hexadecimal
+  static String convertDecimalToHexadecimal(String sequence) {
+    var result = _getResult(sequence, 16);
+    return result;
+  }
+
+  static String convertBinaryToHexadecimal(String sequence) {
+    var decimalSequence = convertBinaryToDecimal(sequence);
+    var result = convertDecimalToHexadecimal(decimalSequence);
+    return result;
+  }
+
+  // Utils
+  static _getResult(String numberText, int radix) {
+    try {
+      var result = int.tryParse(numberText)?.toRadixString(radix).toUpperCase();
+      return result ?? '';
+    } catch (e) {
+      return '';
+    }
+  }
+
+  static _getResultExplicitRadix(String sequence, int radix) {
+    try {
+      var result = int.tryParse(sequence, radix: radix);
+      return result != null ? result.toString() : '';
+    } catch (e) {
+      return '';
+    }
   }
 }
--- lib/view/converter_view.dart
+++ lib/view/converter_view.dart
@@ -19,7 +19,8 @@ class _ConverterViewState extends State<ConverterView> {
 
   List<DropdownMenuItem<String>> menuItems = const [
     DropdownMenuItem(value: 'decimal', child: Text('decimal')),
-    DropdownMenuItem(value: 'binário', child: Text('binary')),
+    DropdownMenuItem(value: 'binário', child: Text('binário')),
+    DropdownMenuItem(value: 'hexadecimal', child: Text('hexadecimal')),
   ];
 
   @override
@@ -40,87 +41,110 @@ class _ConverterViewState extends State<ConverterView> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(16),
-            child: const Text(
-              Constants.appConverterDescription,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(16),
+              child: const Text(
+                Constants.appConverterDescription,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(right: 50, left: 50),
-            child: Column(
-              children: [
-                InputTextWidget(
-                  controller: _controller,
-                  onChanged: (text) {
-                    setState(() {
-                      _controller.value.text;
-                    });
-                  },
-                  hintText: 'Valor a ser convertido',
-                  label: 'Insira um valor para a conversão',
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(height: 16),
-                DropdownButton(
-                  items: menuItems,
-                  value: selectedItem,
-                  onChanged: (value) {
-                    setState(() {
-                      selectedItem = value ?? selectedItem;
-                    });
-                  },
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  'DECIMAL',
-                  textAlign: TextAlign.center,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  color: const Color(0xFFD4D1D1),
-                  child: Center(
-                    child: Text(
-                      getDigits(_controller.value.text, selectedItem)[
-                              'decimal'] ??
-                          '',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+            Container(
+              margin: const EdgeInsets.only(right: 50, left: 50),
+              child: Column(
+                children: [
+                  InputTextWidget(
+                    controller: _controller,
+                    onChanged: (text) {
+                      setState(() {
+                        _controller.value.text;
+                      });
+                    },
+                    hintText: 'Valor a ser convertido',
+                    label: 'Insira um valor para a conversão',
+                    keyboardType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 16),
+                  DropdownButton(
+                    items: menuItems,
+                    value: selectedItem,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedItem = value ?? selectedItem;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'DECIMAL',
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: const Color(0xFFD4D1D1),
+                    child: Center(
+                      child: Text(
+                        getDigits(_controller.value.text, selectedItem)[
+                                'decimal'] ??
+                            '',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  'BINÁRIO',
-                  textAlign: TextAlign.center,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  color: const Color(0xFFD4D1D1),
-                  child: Center(
-                    child: Text(
-                      getDigits(
-                              _controller.value.text, selectedItem)['binary'] ??
-                          '',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+                  const SizedBox(height: 16),
+                  const Text(
+                    'BINÁRIO',
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: const Color(0xFFD4D1D1),
+                    child: Center(
+                      child: Text(
+                        getDigits(_controller.value.text, selectedItem)[
+                                'binary'] ??
+                            '',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 16),
+                  const Text(
+                    'HEXADECIMAL',
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: const Color(0xFFD4D1D1),
+                    child: Center(
+                      child: Text(
+                        getDigits(
+                                _controller.value.text, selectedItem)['hex'] ??
+                            '',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -132,10 +156,17 @@ class _ConverterViewState extends State<ConverterView> {
       case 'decimal':
         values['decimal'] = value;
         values['binary'] = Functions.convertDecimalToBinary(value);
+        values['hex'] = Functions.convertDecimalToHexadecimal(value);
         break;
-      case 'binary':
-        values['decimal'] = Functions.convertBinaryToDecimal(value);
+      case 'binário':
         values['binary'] = value;
+        values['decimal'] = Functions.convertBinaryToDecimal(value);
+        values['hex'] = Functions.convertBinaryToHexadecimal(value);
+        break;
+      case 'hexadecimal':
+        values['hex'] = value;
+        values['decimal'] = Functions.convertHexadecimalToDecimal(value);
+        values['binary'] = Functions.convertHexadecimalToBinary(value);
     }
 
     return values;
